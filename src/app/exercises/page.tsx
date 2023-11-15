9615514ed85a94fcd37667bbbe3f43ad07b38606
Filipe Sales <eduardojs999@gmail.com>
2023-11-15 13:28:13 -0500

    feat: exercicio 02

--- src/app/exercises/page.tsx
+++ src/app/exercises/page.tsx
@@ -12,8 +12,26 @@ export default function Home() {
   const [plagioclasios, setPlagioclasios] = useState("");
   const [isCorrect, setIsCorrect] = useState<boolean | null>(null);
 
+  //exercicio 02
+
+  const [cadeia, setCadeia] = useState("");
+  const [gp2, setGP2] = useState("");
+  const [gp3, setGP3] = useState("");
+  const [gp4, setGP4] = useState("");
+  const [gp5, setGP5] = useState("");
+  const [inossilicatos, setInossilicatos] = useState("");
+  const [hornblenda, setHornblenda] = useState("");
+  const [isCorrectEx2, setIsCorrectEx2] = useState<boolean | null>(null);
+
+  //exercicio 03
+
+  const [cadeia2, setCadeia2] = useState("");
+
   const handleSubmit = (event: any) => {
     event.preventDefault();
+
+    //exercicio 01
+
     const isGpCorrect = gp === "4";
     const isTectossilicatosCorrect =
       tectossilicatos.toLowerCase() === "tectossilicatos";
@@ -22,6 +40,26 @@ export default function Home() {
 
     setIsCorrect(
       isGpCorrect && isTectossilicatosCorrect && isPlagioclasiosCorrect
+    );
+
+    //exercicio 02
+
+    const isCadeiaCorrect = cadeia.toLowerCase() === "dupla";
+    const isGp2Correct = gp2 === "2";
+    const isGp3Correct = gp3 === "3";
+    const isGp4Correct = gp4 === "2";
+    const isGp5Correct = gp5 === "3";
+    const isInossilicatosCorrect = inossilicatos.toLowerCase() === "inossilicatos";
+    const isHornblendaCorrect = hornblenda.toLowerCase() === "hornblenda";
+
+    setIsCorrectEx2(
+      isGp2Correct &&
+      isGp3Correct &&
+      isGp4Correct &&
+      isGp5Correct &&
+      isCadeiaCorrect &&
+        isInossilicatosCorrect &&
+        isHornblendaCorrect
     );
   };
 
@@ -96,138 +134,98 @@ export default function Home() {
                 {isCorrect ? "Correct" : "Incorrect"}
               </div>
             )}
-            <div className="mt-10 max-w-2xl">
-              <p>
-                Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget
-                risus enim. Mattis mauris semper sed amet vitae sed turpis id.
-                Id dolor praesent donec est. Odio penatibus risus viverra tellus
-                varius sit neque erat velit. Faucibus commodo massa rhoncus,
-                volutpat. Dignissim sed eget risus enim. Mattis mauris semper
-                sed amet vitae sed turpis id.
-              </p>
-              <ul role="list" className="mt-8 max-w-xl space-y-8 text-gray-600">
-                <li className="flex gap-x-3">
-                  <CheckCircleIcon
-                    className="mt-1 h-5 w-5 flex-none text-indigo-600"
+
+            <div className="flex flex-col items-center">
+              <figure className="mt-16">
+                <img
+                  className="aspect-video rounded-xl bg-gray-50 object-cover"
+                  src="images/exercicio02.png"
+                  alt=""
+                />
+                <figcaption className="mt-4 flex gap-x-2 text-sm leading-6 text-gray-500">
+                  <InformationCircleIcon
+                    className="mt-0.5 h-5 w-5 flex-none text-gray-300"
                     aria-hidden="true"
                   />
-                  <span>
-                    <strong className="font-semibold text-gray-900">
-                      Data types.
-                    </strong>{" "}
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Maiores impedit perferendis suscipit eaque, iste dolor
-                    cupiditate blanditiis ratione.
-                  </span>
-                </li>
-                <li className="flex gap-x-3">
-                  <CheckCircleIcon
-                    className="mt-1 h-5 w-5 flex-none text-indigo-600"
-                    aria-hidden="true"
-                  />
-                  <span>
-                    <strong className="font-semibold text-gray-900">
-                      Loops.
-                    </strong>{" "}
-                    Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
-                    qui lorem cupidatat commodo.
-                  </span>
-                </li>
-                <li className="flex gap-x-3">
-                  <CheckCircleIcon
-                    className="mt-1 h-5 w-5 flex-none text-indigo-600"
-                    aria-hidden="true"
-                  />
-                  <span>
-                    <strong className="font-semibold text-gray-900">
-                      Events.
-                    </strong>{" "}
-                    Ac tincidunt sapien vehicula erat auctor pellentesque
-                    rhoncus. Et magna sit morbi lobortis.
-                  </span>
-                </li>
-              </ul>
-              <p className="mt-8">
-                Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis
-                odio id et. Id blandit molestie auctor fermentum dignissim.
-                Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate
-                et ultrices hac adipiscing egestas. Iaculis convallis ac tempor
-                et ut. Ac lorem vel integer orci.
-              </p>
-              <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">
-                From beginner to expert in 3 hours
-              </h2>
-              <p className="mt-6">
-                Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam
-                consequat in. Convallis arcu ipsum urna nibh. Pharetra, euismod
-                vitae interdum mauris enim, consequat vulputate nibh. Maecenas
-                pellentesque id sed tellus mauris, ultrices mauris. Tincidunt
-                enim cursus ridiculus mi. Pellentesque nam sed nullam sed diam
-                turpis ipsum eu a sed convallis diam.
-              </p>
-              <figure className="mt-10 border-l border-indigo-600 pl-9">
-                <blockquote className="font-semibold text-gray-900">
-                  <p>
-                    “Vel ultricies morbi odio facilisi ultrices accumsan donec
-                    lacus purus. Lectus nibh ullamcorper ac dictum justo in
-                    euismod. Risus aenean ut elit massa. In amet aliquet eget
-                    cras. Sem volutpat enim tristique.”
-                  </p>
-                </blockquote>
-                <figcaption className="mt-6 flex gap-x-4">
-                  <img
-                    className="h-6 w-6 flex-none rounded-full bg-gray-50"
-                    src="https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                  />
-                  <div className="text-sm leading-6">
-                    <strong className="font-semibold text-gray-900">
-                      Maria Hill
-                    </strong>{" "}
-                    – Marketing Manager
-                  </div>
+                  Descrição da imagem.
                 </figcaption>
               </figure>
-              <p className="mt-10">
-                Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget
-                risus enim. Mattis mauris semper sed amet vitae sed turpis id.
-                Id dolor praesent donec est. Odio penatibus risus viverra tellus
-                varius sit neque erat velit.
-              </p>
-            </div>
-            <figure className="mt-16">
-              <img
-                className="aspect-video rounded-xl bg-gray-50 object-cover"
-                src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&auto=format&fit=facearea&w=1310&h=873&q=80&facepad=3"
-                alt=""
+              <h2 className="mt-10">Subclasse</h2>
+              <input
+                name="gp2"
+                id="gp2"
+                value={gp2}
+                onChange={(e) => setGP2(e.target.value)}
+                className="mt-2 block w-auto rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="GP2"
               />
-              <figcaption className="mt-4 flex gap-x-2 text-sm leading-6 text-gray-500">
-                <InformationCircleIcon
-                  className="mt-0.5 h-5 w-5 flex-none text-gray-300"
-                  aria-hidden="true"
+              <input
+                name="gp3"
+                id="gp3"
+                value={gp3}
+                onChange={(e) => setGP3(e.target.value)}
+                className="mt-2 block w-auto rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="GP3"
+              />
+              <input
+                name="gp4"
+                id="gp4"
+                value={gp4}
+                onChange={(e) => setGP4(e.target.value)}
+                className="mt-2 block w-auto rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="GP4"
+              />
+              <input
+                name="gp5"
+                id="gp5"
+                value={gp5}
+                onChange={(e) => setGP5(e.target.value)}
+                className="mt-2 block w-auto rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="GP5"
+              />
+              <input
+                name="cadeia2"
+                id="cadeia2"
+                value={cadeia}
+                onChange={(e) => setCadeia(e.target.value)}
+                className="mt-2 block w-auto rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="cadeia"
+              />
+              <input
+                name="Inossilicatos"
+                id="Inossilicatos"
+                value={inossilicatos}
+                onChange={(e) => setInossilicatos(e.target.value)}
+                className="mt-5 mb-5 block w-auto rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="Inossilicatos"
+              />
+              <div className="flex flex-col items-center">
+                <input
+                  name="Hornblenda"
+                  id="Hornblenda"
+                  value={hornblenda}
+                  onChange={(e) => setHornblenda(e.target.value)}
+                  className="w-auto rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="Hornblenda"
                 />
-                Faucibus commodo massa rhoncus, volutpat.
-              </figcaption>
-            </figure>
-            <div className="mt-16 max-w-2xl">
-              <h2 className="text-2xl font-bold tracking-tight text-gray-900">
-                Everything you need to get up and running
-              </h2>
-              <p className="mt-6">
-                Purus morbi dignissim senectus mattis adipiscing. Amet, massa
-                quam varius orci dapibus volutpat cras. In amet eu ridiculus leo
-                sodales cursus tristique. Tincidunt sed tempus ut viverra
-                ridiculus non molestie. Gravida quis fringilla amet eget dui
-                tempor dignissim. Facilisis auctor venenatis varius nunc, congue
-                erat ac. Cras fermentum convallis quam.
-              </p>
-              <p className="mt-8">
-                Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget
-                risus enim. Mattis mauris semper sed amet vitae sed turpis id.
-                Id dolor praesent donec est. Odio penatibus risus viverra tellus
-                varius sit neque erat velit.
-              </p>
+                <span className="block text-center mt-2">
+                  (Na,Ca)AlSi<sub>2</sub>(Si,Al)O<sub>8</sub>
+                </span>
+              </div>
+
+              <button
+                onClick={handleSubmit}
+                className="bg-red-900 text-white px-4 py-2 rounded-md mt-4 hover:bg-red-800"
+              >
+                Submit
+              </button>
             </div>
+
+            {isCorrectEx2 !== null && (
+              <div className={`text-${isCorrectEx2 ? "green" : "red"}-500`}>
+                {isCorrectEx2 ? "Correct" : "Incorrect"}
+              </div>
+            )}
           </div>
         </div>
       </main>
